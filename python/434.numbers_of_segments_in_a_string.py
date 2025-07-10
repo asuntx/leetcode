50880d1206521c24fc6283371475cd3fb184c78b
Sebastian Figueroa <sebaj348@gmail.com>
2025-07-10 13:53:15 -0400

    fix commit

--- python/434.numbers_of_segments_in_a_string.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def countSegments(self, s: str) -> int:
-        return len(s.split(" "))
-
-
-sol = Solution()
-print(sol.countSegments("Hello, word tomate is  big"))
--- python/522.longest_uncommon_subsequence_II.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-
-class Solution:
-    def findLUSlength(self, strs: List[str]) -> int:
-sol = Solution()
-print(sol.findLUSlength(["aabbcc", "aabbcc", "cb", "abc"]))
