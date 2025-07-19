e253f2413d647028191d5166df409c3d46039c96
Sebastian Figueroa <sebaj348@gmail.com>
2025-07-19 10:36:42 -0400

    771

---
+++ python/771.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        res = 0
+        for stone in stones:
+            if stone in jewels:
+                res += 1
+        return res
+
+
+sol = Solution()
+print(sol.numJewelsInStones(jewels="aA", stones="aAAbbbb"))
